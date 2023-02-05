fa706155b7e4c1eb01eeba5d4a9b0f261dba0579
gabriel da silva pires <mr.boostphoenix@gmail.com>
2023-02-05 14:26:36 -0300

    adicionando arquivos ao repositório remoto

--- src/config/connect-db.php
+++ src/config/connect-db.php
@@ -2,11 +2,6 @@
     $hostname = "localhost";
     $username = "root";
     $password = "";
-    $db = "sga_empregos_db";
-
+    $db = "sga_empresas_db";
     $conn = new mysqli($hostname, $username, $password, $db);
-    
-    if ($conn->connect_error) {
-        echo "Falha ao conectar! Erro: ".($conn->connect_error);
-    }
 ?>
