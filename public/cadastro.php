b49ad7bb40dd3f1ead682f4e346b300841b7a66d
andersonzero0 <programadorzero@hotmail.com>
2023-02-06 20:30:23 -0300

    style cadastro.html add

--- public/cadastro.php
+++ public/cadastro.php
@@ -4,17 +4,33 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title></title>
+
+    <link rel="stylesheet" href="assets/css/cadastro.css">
+    <link rel="shortcut icon" href="assets/images/favicon.ico" type="image/x-icon">
+    <script src="https://kit.fontawesome.com/05378ddbee.js" crossorigin="anonymous"></script>
+
+    <title>Cadastro - SGA Empregos</title>
 </head>
 <body>
+
     <main>
-        <form action="<?= htmlspecialchars('src/controllers/cadastroControllers.php') ?>" method="POST">
-            <label for="">Usuário</label>
-            <input type="text" minlength="5" maxlength="15" required name="userNameClient">
-            <label for="">Senha</label>
-            <input type="password" minlength="10" maxlength="15" required name="passwordClient">
-            <input type="submit" value="Enviar">
+        <form class="conteiner-form" action="<?= htmlspecialchars('src/controllers/cadastroControllers.php') ?>" method="POST">
+
+            <div class="conteiner-legend">
+                <legend class="legend-form">CADASTRO</legend>
+            </div>
+        
+            <div class="content-input">
+                <input class="input-form" type="text" minlength="5" maxlength="15" required name="userNameClient" placeholder="Nome de usuário">
+            </div>
+
+            <div class="content-input">
+                <input class="input-form" type="password" minlength="10" maxlength="15" required name="passwordClient" placeholder="Senha">
+            </div>
+
+            <div><input class="btn_submit" type="submit" value="Cadastrar"></div>
         </form>
     </main>
+
 </body>
 </html>
