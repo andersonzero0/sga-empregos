ebcf1debae8cf6e543cc53e93224c4e64e245820
andersonzero0 <programadorzero@hotmail.com>
2023-02-18 16:24:35 -0300

    update

--- public/src/views/recrutadorView.php
+++ public/src/views/recrutadorView.php
@@ -3,6 +3,8 @@
         header('location: ../../index.html');
     } else {
 
+        /* code php*/
+
 ?>
 
 <main>
@@ -10,7 +12,25 @@
 
         <form action="<?= htmlspecialchars('') ?>" method="get">
 
+            <label for="cargo">Cargo:</label>
+            <input type="text" name="cargo" id="cargo">
             
+            <label for="empresa">Empresa:</label>
+            <input type="text" name="empresa" id="empresa">
+
+            <label for="local">Local:</label>
+            <input type="text" name="local" id="local">
+
+            <label for="turno">Turno:</label>
+            <input type="text" name="turno" id="turno">
+
+            <label for="qnt_vaga">Quantidade de vagas:</label>
+            <input type="text" name="qnt_vaga" id="qnt_vaga">
+
+            <label for="decricao_vaga"></label>
+            <textarea name="descricao_vaga" id="descricao" cols="100" rows="20"></textarea>
+
+            <button name="submit" type="submit">Criar vaga</button>
             
         </form>
         
