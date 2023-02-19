acd90d0d770099c6ea5cb57090d1034f8f493a46
andersonzero0 <programadorzero@hotmail.com>
2023-02-19 13:17:08 -0300

    Create table "registrocandit_tb"
    Co-authored-by: Gabriel Pires <gabrielboostphoenix@users.noreply.github.com>

--- public/src/db/sga_empregos_db.sql
+++ public/src/db/sga_empregos_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 18-Fev-2023 às 21:02
+-- Tempo de geração: 19-Fev-2023 às 17:15
 -- Versão do servidor: 10.4.24-MariaDB
 -- versão do PHP: 8.1.6
 
@@ -24,6 +24,20 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Estrutura da tabela `registrocandit_tb`
+--
+
+CREATE TABLE `registrocandit_tb` (
+  `id` int(11) NOT NULL,
+  `forward_key_user` int(11) NOT NULL,
+  `forward_key_vaga` int(11) NOT NULL,
+  `stage_vaga` varchar(15) NOT NULL,
+  `data_regist` varchar(12) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
 -- Estrutura da tabela `users_tb`
 --
 
@@ -40,8 +54,8 @@ CREATE TABLE `users_tb` (
 --
 
 INSERT INTO `users_tb` (`id_user`, `user_name`, `password_user`, `token_user`, `date_user`) VALUES
-(1, 'usertest', 'b434d5c737faf126c98ccd91132f6892', '4c59224e2fc5d8f1c9b98c3be81d9974', '17/02/2023'),
-(2, 'usertest1', 'b434d5c737faf126c98ccd91132f6892', 'f26f1beccc4933abfa9036b03b3d0848', '18/02/2023');
+(1, 'usertest', 'b434d5c737faf126c98ccd91132f6892', '7cfe4ee5675ecba21fa20b63c305bfef', '19/02/2023'),
+(2, 'userCandidato', 'b434d5c737faf126c98ccd91132f6892', 'cc494e3334186f2048f6188276c49ea1', '19/02/2023');
 
 -- --------------------------------------------------------
 
@@ -66,7 +80,8 @@ CREATE TABLE `user_datas_tb` (
 --
 
 INSERT INTO `user_datas_tb` (`id_userData`, `forward_key_userData`, `full_name_userData`, `email_address_userData`, `phone_number_userData`, `type_user`, `external_link_userData`, `link_image_profile_userData`, `link_curriculum_vitae_userData`) VALUES
-(1, 1, 'User Test User Test', 'user@test', '01010101010101010101', 'recrutador', 'http://usertest.com', 'wallhaven-j3d7y5.jpg', 'fceb33c6-d63d-4c7b-b343-1a3344e1d34c.pdf');
+(1, 1, 'Usuario Test 2sasa', 'assaa@dsdsa', '11212121121212121121', 'Recrutador', 'http://sasas', 'user.png', 'fceb33c6-d63d-4c7b-b343-1a3344e1d34c.pdf'),
+(2, 2, 'User user user user', 'dsd@ddsdsad', '42342343434332', 'Candidato', 'http://asasas', 'user.png', 'fceb33c6-d63d-4c7b-b343-1a3344e1d34c.pdf');
 
 -- --------------------------------------------------------
 
@@ -86,8 +101,21 @@ CREATE TABLE `vagas_tb` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
+-- Extraindo dados da tabela `vagas_tb`
+--
+
+INSERT INTO `vagas_tb` (`id_vaga`, `forward_key_vaga`, `cargo_vaga`, `empresa_vaga`, `local_vaga`, `turno_vaga`, `qnt_vaga`, `descricao_vaga`) VALUES
+(0, 1, 'Engenheiro de Software', 'Microsoft', 'New York, EUA', 'Integral', 12, 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sapiente non facere aliquid eum impedit aperiam beatae explicabo. Fugit officia iure fugiat ea minima totam assumenda laborum, earum aperiam nihil ');
+
+--
 -- Índices para tabelas despejadas
 --
+
+--
+-- Índices para tabela `registrocandit_tb`
+--
+ALTER TABLE `registrocandit_tb`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Índices para tabela `users_tb`
@@ -121,7 +149,7 @@ ALTER TABLE `users_tb`
 -- AUTO_INCREMENT de tabela `user_datas_tb`
 --
 ALTER TABLE `user_datas_tb`
-  MODIFY `id_userData` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id_userData` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
